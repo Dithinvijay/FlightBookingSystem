5df153f6adda18e35ed349ddd8ed616501c604d4
Ditinvijay <dithin12398@gmail.com>
2025-06-25 00:54:08 +0530

    Sky is dark

--- Frontend/VKFlights/src/app/home/home.component.ts
+++ Frontend/VKFlights/src/app/home/home.component.ts
@@ -1,8 +1,41 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  imports: [NgIf, FormsModule]
 })
-export class HomeComponent {}
+export class HomeComponent {
+  isActive = false;
+  isFinished = false;
+  isLoading = false;
+
+  fromValue = '';
+  toValue = '';
+
+  constructor(private router: Router) {}
+
+  onAnimatedSearchClick() {
+    if (this.isActive || this.isLoading) return;
+    this.isActive = true;
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isActive = false;
+      this.isFinished = true;
+      this.isLoading = false;
+      setTimeout(() => {
+        this.router.navigate(['/deals']);
+      }, 800); // Show checkmark before redirect
+    }, 1800); // Animation duration
+  }
+
+  swapFromTo() {
+    const temp = this.fromValue;
+    this.fromValue = this.toValue;
+    this.toValue = temp;
+  }
+}
