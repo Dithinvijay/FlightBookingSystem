4d01fa8a718b89e89c5902e1c2b492524f841187
Ditinvijay <dithin12398@gmail.com>
2025-06-24 14:16:00 +0530

    Sky is red

---
+++ Frontend/VKFlights/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+// This component serves as the root component for the Angular application
+export class AppComponent { //these are properties of the AppComponent class
+  title = 'VKFlights'; // The title of the application
+  name= 'Dithin';
+  
+}
